d154d7d3ebc53be9110236296dbad484c62be993
chrisBalher <c.baldeon.h@gmail.com>
2021-01-31 09:54:45 -0500

    arreglado para githubpages

---
+++ assets/scripts/App.js
@@ -0,0 +1,15 @@
+import "../styles/styles.css"
+import 'lazysizes'
+/* import MobileMenu from "./modules/MobileMenu.js"
+ */
+
+
+
+let modal
+let stickyheader = new StickyHeader
+let mobilemenu = new MobileMenu()
+
+
+if(module.hot){
+    module.hot.accept()
+}
