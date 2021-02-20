3654b3191c43a187c902cd5c04326f57edf14847
chrisBalher <c.baldeon.h@gmail.com>
2021-02-19 19:44:32 -0500

    Terminado vista movil

--- assets/scripts/modules/menuButton.js
+++ assets/scripts/modules/menuButton.js
@@ -1,10 +1,33 @@
 class menuButton {
     constructor(){
         this.menuBTN = document.getElementById("menu-btn")
-        this.ventana= document.body
+        this.ventana = document.getElementById("menu-nav")
         this.events()
+        this.count1 =0
+        this.count2 =0
     }
     events(){
-        this.ventana.addEventListener('click',console.log("Te escucho"))
+        this.menuBTN.addEventListener('click',()=>this.cerrar())
+        this.ventana.addEventListener('click',()=>this.clickVentana())
+        
+    }
+    
+    clickVentana(){
+        if (this.menuBTN.checked == true ){
+                this.menuBTN.checked =false
+                this.count2 = 0
+                this.count1 = 0
+        }
+    }
+    cerrar(){
+        if(this.menuBTN.checked==true){
+            this.count2 = this.count2+1
+            if (this.count2 > 1) {
+                this.menuBTN.checked =false
+                this.count2 = 0
+                this.count1 = 0
+            }
+            this.count2 = 0
+        }
     }
 }export default menuButton
