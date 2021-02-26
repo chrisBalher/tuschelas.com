a2568575816b1ec322685b0e410015bd319b9173
chrisBalher <c.baldeon.h@gmail.com>
2021-02-26 11:47:16 -0500

    Productos

--- assets/scripts/modules/menuButton.js
+++ assets/scripts/modules/menuButton.js
@@ -1,22 +1,20 @@
 class menuButton {
     constructor(){
         this.menuBTN = document.getElementById("menu-btn")
-        this.ventana = document.getElementById("menu-nav")
+        this.menuNav = document.getElementById("menu-nav")
         this.events()
-        this.count1 =0
         this.count2 =0
     }
     events(){
         this.menuBTN.addEventListener('click',()=>this.cerrar())
-        this.ventana.addEventListener('click',()=>this.clickVentana())
+        this.menuNav.addEventListener('click',()=>this.clickmenuNav())
         
     }
     
-    clickVentana(){
+    clickmenuNav(){
         if (this.menuBTN.checked == true ){
                 this.menuBTN.checked =false
                 this.count2 = 0
-                this.count1 = 0
         }
     }
     cerrar(){
@@ -25,7 +23,6 @@ class menuButton {
             if (this.count2 > 1) {
                 this.menuBTN.checked =false
                 this.count2 = 0
-                this.count1 = 0
             }
             this.count2 = 0
         }
